1ada9df50afd02762767967b05528490c5875aac
JINO-ROHIT <find.jinorohit@gmail.com>
2026-03-21 11:11:42 +0530

    feat: use_cache flag + fix no cache output bug

--- tachyon/engine/llm.py
+++ tachyon/engine/llm.py
@@ -24,23 +24,30 @@ class Engine:
         
         return next_token
 
-    def generate(self, prompt: str, max_tokens: int = 100, temperature: float = 0.0):
-        cache = Cache(n_layers=16)
+    def generate(self, prompt: str, max_tokens: int = 100, temperature: float = 0.0, use_cache: bool = True):
+        if use_cache:
+            cache = Cache(n_layers=16)
+        else:
+            cache = None
         self.model.current_pos = 0  # reset position for each new generation
 
         # prefill
         tokens = torch.tensor(self.tokenizer.encode(prompt)).unsqueeze(0).to(device)
         with torch.no_grad():
             logits = self.model(tokens, cache=cache)
-        
+
         next_token = self.sample(logits[:, -1, :], temperature)
-        
+
         # decode only feed ONE token at a time, cache does the rest
         generated = [next_token.item()]
         for _ in range(max_tokens - 1):
             with torch.no_grad():
-                logits = self.model(next_token, cache=cache)
-            
+                if use_cache:
+                    logits = self.model(next_token, cache=cache)
+                else: # if no cache, take the prefill and append to each decode and then pass the whole thing each timestep
+                    tokens = torch.cat([tokens, next_token], dim=1)
+                    logits = self.model(tokens, cache=None)
+
             next_token = self.sample(logits[:, -1, :], temperature)
             generated.append(next_token.item())
 
@@ -50,9 +57,10 @@ class Engine:
         prompt_ids = tokens.squeeze(0).tolist()
         return self.tokenizer.decode(prompt_ids + generated)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     engine = Engine("meta-llama/Llama-3.2-1B-Instruct")
     toks = engine.generate("hey man", 50)
     print(toks)
-    toks = engine.generate("whats good homie?", 50)
+    toks = engine.generate("whats good homie?", 50, use_cache=False)
     print(toks)
